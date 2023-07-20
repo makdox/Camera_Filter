7e588b2f2f4f415553625c30f8c455f87bd4ccf2
Maksym Donets <timnirton7@gmail.com>
2023-07-20 22:49:41 +0300

    Initial Commit

---
+++ Camera_Filter/Camera_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_FilterApp.swift
+//  Camera_Filter
+//
+//  Created by Max Donets on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Camera_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
